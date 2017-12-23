2c686368d85f3f9c9699a7f1142e666ce9007ae0
Edward <gonelove.guo@gmail.com>
2017-12-23 23:24:03 +0800

    init

---
+++ src/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>{{msg}}</h1>
+    <Hello/>
+  </div>
+</template>
+
+<script>
+import Hello from './components/hello';
+export default {
+  data() {
+    return {
+      msg: 'Parcels'
+    };
+  },
+  components: {
+    Hello
+  }
+};
+</script>
+
+<style>
+  div {
+    background: red;
+  }
+</style>
